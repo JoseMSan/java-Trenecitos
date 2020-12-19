975e9dfa80509b4847b26cea2a5bb84d27a53617
Jose <jsantp01@estudiantes.unileon.es>
2020-12-19 22:22:27 +0100

    Working version with train objects implemented. Only movement left.

--- Trenecitos/src/Trenes/MainTrenes.java
+++ Trenecitos/src/Trenes/MainTrenes.java
@@ -3,40 +3,50 @@ package Trenes;
 import java.util.Scanner;
 
 public class MainTrenes {
+    static int contTren=0;
     public static void main (String[] args){
         Casilla [][] tablero = new Casilla[30][30];
         generarTablero(tablero);
-        imprimirTablero(tablero);
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String aux = sc.nextLine(); // Elimina el /n sobrante
-        if(!(n>=1 && n<=10)) {
-            error();
-        }
-        String []datosTrenes = extraeDatosTrenes(n,sc);
-        colocarTrenes(datosTrenes, tablero);
+//        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            String aux = sc.nextLine(); // Elimina el /n sobrante
+            if (!(n >= 1 && n <= 10)) {
+                error();
+            }
+            Trenes [] trenes = new Trenes[n];
+            String[] datosTrenes = extraeDatosTrenes(n, sc);
+            colocarTrenes(datosTrenes, tablero, trenes);
+//            simulacion(tablero);
 
-
-
-        imprimirTablero(tablero);
+            imprimirTablero(tablero,trenes);
+//        }
         sc.close();
     }
 
-    public static void colocarTrenes(String []datosTrenes, Casilla [][] tablero) {
+//    public static void simulacion(Casilla [][] tablero) {
+//        for (int i = 0; i < 30; i++){
+//            for (int j = 0; j < 30; j++){
+//
+//            }
+//        }
+//    }
+
+    public static void colocarTrenes(String []datosTrenes, Casilla [][] tablero, Trenes[] trenes) {
         for (String datosTrene : datosTrenes) {
             String[] datos = datosTrene.split(" ");
             switch (datos[0]) {
                 case "B":
-                    dibujarTrenes("0", datos[1], datos[2], datos[3], tablero);
+                    dibujarTrenes("0", datos[1], datos[2], datos[3], tablero, trenes);
                     break;
                 case "A":
-                    dibujarTrenes("1", datos[1], datos[2], datos[3], tablero);
+                    dibujarTrenes("1", datos[1], datos[2], datos[3], tablero, trenes);
                     break;
                 case "I":
-                    dibujarTrenes("2", datos[1], datos[2], datos[3], tablero);
+                    dibujarTrenes("2", datos[1], datos[2], datos[3], tablero, trenes);
                     break;
                 case "D":
-                    dibujarTrenes("3", datos[1], datos[2], datos[3], tablero);
+                    dibujarTrenes("3", datos[1], datos[2], datos[3], tablero, trenes);
                     break;
                 default:
                     error();
@@ -44,48 +54,88 @@ public class MainTrenes {
         }
     }
 
-    public static void dibujarTrenes(String tipo, String tam, String val1, String val2, Casilla[][] tablero) {
+    public static void dibujarTrenes(String tipo, String size, String val1, String val2, Casilla[][] tablero, Trenes[] trenes) {
         int x = Integer.parseInt(val1);
         int y = Integer.parseInt(val2);
-        int i;
-        if(!tablero[y][x].estado.equals(".")){
+        int tam = Integer.parseInt(size);
+
+        trenes[contTren] = new Trenes(tam,Integer.parseInt(tipo));
+
+        if(!tablero[y][x].getEstado().equals(".")){
             error();
         }
-        tablero[y][x].estado=tipo;
-        switch (tablero[y][x].estado) {
+        int i,cont;
+        switch (trenes[contTren].tipo) {
             case "0":
-                if((y+Integer.parseInt(tam)-1)>29){
+                cont = y + tam - 1;
+                if(cont>29){
                     error();
                 }
-                for (i = (y + Integer.parseInt(tam)-1); i >= y; i--) {
-                    tablero[i][x].estado=tipo;
+                for (i =0;i<tam;i++) {
+                    trenes[contTren].vagones[i][1]=cont;
+                    trenes[contTren].vagones[i][0]=x;
+                    cont--;
+                }
+                for(int j=0;j<tam;j++) {
+                    for ( int h = 0; h < 2; h++) {
+                        System.out.printf("[%d]",trenes[contTren].vagones[j][h]);
+                    }
+                    System.out.print("\n");
                 }
                 break;
             case "1":
-                if((y-Integer.parseInt(tam)+1)<0){
+                cont = y - tam + 1;
+                if(cont<0){
                     error();
                 }
-                for (i = (y - Integer.parseInt(tam)+1); i <= y; i++) {
-                    tablero[i][x].estado=tipo;
+                for (i =0;i<tam;i++) {
+                    trenes[contTren].vagones[i][0]=x;
+                    trenes[contTren].vagones[i][1]=cont;
+                    cont++;
+                }
+                for(int j=0;j<tam;j++) {
+                    for ( int h = 0; h < 2; h++) {
+                        System.out.printf("[%d]",trenes[contTren].vagones[j][h]);
+                    }
+                    System.out.print("\n");
                 }
                 break;
             case "2":
-                if((x+Integer.parseInt(tam)-1)>29){
+                cont = x + tam - 1;
+                if(cont>29){
                     error();
                 }
-                for (i = (x + Integer.parseInt(tam)-1); i >= x; i--) {
-                    tablero[y][i].estado=tipo;
+                for (i =0;i<tam;i++) {
+                    trenes[contTren].vagones[i][1]=y;
+                    trenes[contTren].vagones[i][0]=cont;
+                    cont--;
+                }
+                for(int j=0;j<tam;j++) {
+                    for ( int h = 0; h < 2; h++) {
+                        System.out.printf("[%d]",trenes[contTren].vagones[j][h]);
+                    }
+                    System.out.print("\n");
                 }
                 break;
             case "3":
-                if((x-Integer.parseInt(tam)+1)<0){
+                cont = x - tam + 1;
+                if(cont<0){
                     error();
                 }
-                for (i = (x - Integer.parseInt(tam)+1); i <= x; i++) {
-                    tablero[y][i].estado=tipo;
+                for (i =0;i<tam;i++) {
+                    trenes[contTren].vagones[i][0]=cont;
+                    trenes[contTren].vagones[i][1]=y;
+                    cont++;
+                }
+                for(int j=0;j<tam;j++) {
+                    for ( int h = 0; h < 2; h++) {
+                        System.out.printf("[%d]",trenes[contTren].vagones[j][h]);
+                    }
+                    System.out.print("\n");
                 }
                 break;
         }
+        contTren++;
     }
 
     public static String[] extraeDatosTrenes(int n, Scanner sc){
@@ -96,12 +146,12 @@ public class MainTrenes {
         return datosTrenes;
     }
 
-    public static void imprimirTablero(Casilla[][] tablero) {
+    public static void imprimirTablero(Casilla[][] tablero, Trenes [] trenes) {
         System.out.print("  ");
         for (int k = 0;k<30;k++) {
             if(k<10){
                 System.out.print(" 0");
-            }else if(k>=10 && k<20){
+            }else if(k<20){
                 System.out.print(" 1");
             }else {
                 System.out.print(" 2");
@@ -112,6 +162,15 @@ public class MainTrenes {
             System.out.print(" 0 1 2 3 4 5 6 7 8 9");
         }
         System.out.print("\n");
+        int piv = contTren;
+        contTren=0;
+
+        for (int k = 0; k < piv; k++) {
+            for(int i = 0; i<trenes[k].tam;i++){
+//                System.out.println("Tren " + k + " tipo " +trenes[k].getTipo() +" mide " + trenes[k].tam);
+                    tablero[trenes[k].vagones[i][1]][trenes[k].vagones[i][0]].setEstado(trenes[k].getTipo());
+            }
+        }
 
         for (int i = 0; i < 30; i++) {
             if((29-i)>=10){
@@ -123,7 +182,11 @@ public class MainTrenes {
             }
             System.out.print("\n");
         }
+
+
+
     }
+
 
     public static void generarTablero(Casilla[][] tablero) {
         for (int i = 0; i < 30; i++) {
--- Trenecitos/src/Trenes/Trenes.java
+++ Trenecitos/src/Trenes/Trenes.java
@@ -3,8 +3,28 @@ package Trenes;
 
 public class Trenes {
     int [][] vagones;
+    String tipo;
+    int tam;
 
-    public Trenes(int tam) {
+    public Trenes(int tam, int tipo) {
         this.vagones = new int[tam][2];
+        this.tipo = String.valueOf(tipo);
+        this.tam = tam;
+    }
+
+    public int[][] getVagones() {
+        return vagones;
+    }
+
+    public void setVagones(int[][] vagones) {
+        this.vagones = vagones;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = String.valueOf(tipo);
     }
 }
